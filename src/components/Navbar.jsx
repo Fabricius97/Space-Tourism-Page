3953be905e5cabacba3170db019fd76e973d83ed
Fabricius97 <christoffer.fabricius@hotmail.com>
2024-12-15 18:50:48 +0100

    Styling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/shared/logo.svg";
+import "../design/Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <ul>
+      <nav className="navbar">
+        <img src={Logo} alt="logo" className="img-logo" />
+        <hr />
+        <ul className="navbar-links">
           <li>
-            <Link to="/">00 Home</Link>
+            <Link to="/">
+              00 <span style={{ opacity: "0.8" }}>HOME</span>
+            </Link>
           </li>
           <li>
-            <Link to="/destination">01 Destination</Link>
+            <Link to="/destination">
+              01 <span style={{ opacity: "0.8" }}>DESTINATION</span>
+            </Link>
           </li>
           <li>
-            <Link to="/crew">02 Crew</Link>
+            <Link to="/crew">
+              02 <span style={{ opacity: "0.8" }}>CREW</span>
+            </Link>
           </li>
           <li>
-            <Link to="/technology">03 Technology</Link>
+            <Link to="/technology">
+              03 <span style={{ opacity: "0.8" }}>TECHNOLOGY</span>
+            </Link>
           </li>
         </ul>
       </nav>
