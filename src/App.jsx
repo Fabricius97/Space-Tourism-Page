f2c94700a62e182233685cc2e8734a86ecd03fc0
Fabricius97 <christoffer.fabricius@hotmail.com>
2024-12-15 15:02:31 +0100

    Getting structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+// App.js
 import React from "react";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          {/* Föräldrarutt med Layout */}
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="destination" element={<Destination />} />
+            <Route path="crew" element={<Crew />} />
+            <Route path="technology" element={<Technology />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">00 Home</Link>
+          </li>
+          <li>
+            <Link to="/destination">01 Destination</Link>
+          </li>
+          <li>
+            <Link to="/crew">02 Crew</Link>
+          </li>
+          <li>
+            <Link to="/technology">03 Technology</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
